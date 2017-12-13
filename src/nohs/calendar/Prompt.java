35e534946a8f545db57db468bd04766e2f22bdd8
fornet <fornet@fornet-PC>
2017-12-13 13:32:02 +0900

    initial prompt

--- src/nohs/calendar/Prompt.java
+++ src/nohs/calendar/Prompt.java
@@ -1,10 +1,39 @@
 package nohs.calendar;
 
+import java.util.Scanner;
+
 public class Prompt {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void runPrompt() {
+
+		String PROMPT = "Cal> ";
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+
+		int month = 1;
+
+		while (true) {
+			System.out.println("달을 입력하세요");
+			System.out.print(PROMPT);
+			month = scanner.nextInt();
+			if (month == -1) {
+				break;
+			}
+			if (month > 12) {
+				continue;
+			}
+			System.out.printf("%d월은 %d일까지 있습니다\n", month, cal.getMaxDaysOfMonth(month));
+		}
+		System.out.println("BYE~");
+		scanner.close();
 
 	}
 
+	private final static String PROMPT = "cal> ";
+
+	public static void main(String[] args) {
+
+		// 숫자를 입력받아 해당하는 달의 최대 월수는 출력하는 프로그램
+
+	}
 }
