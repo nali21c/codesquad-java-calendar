047b9c46257a0682e1bbf2ac17af6c9e6ddeec71
fornet <fornet@fornet-PC>
2017-12-13 11:39:07 +0900

    calendar modification

--- src/nohs/calendar/Calendar.java
+++ src/nohs/calendar/Calendar.java
@@ -3,28 +3,35 @@ package nohs.calendar;
 import java.util.Scanner;
 
 public class Calendar {
+	private final int[] Max_Date = { 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30 };
+
+	public int getMaxDaysOfMonth(int month) {
+		return Max_Date[month - 1];
+	}
 
 	public static void main(String[] args) {
 
-		System.out.println("일  월  화 수 목 금  토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+		System.out.println("반복 횟수를 입력하세요");
+		Scanner repeat = new Scanner(System.in);
+		int time = repeat.nextInt();
+		
 
-		// 월을 입력하면 그 달이 몇일로 구성되어 있는지 출력하는 프로그램
+		System.out.println("월을 입력하세요");
+		Calendar cal = new Calendar();
 
-		Scanner scanner = new Scanner(System.in);
-		int a;
-		System.out.println("달을 입력하세요");
-		String month = scanner.nextLine();
-		a = Integer.parseInt(month);
+		int[] month = new int[time - 1];
 
-		int[] b = { 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30 };
-
-		System.out.println(b[a-1]);
-		System.out.println(a);
+		for (int i = 0; i <= month.length; i++) {
+			Scanner scanner = new Scanner(System.in);
+			month[i] = scanner.nextInt();
+		
+		}
+		 
+		 for(int j=0; j <= month.length; j++) {
+		 System.out.printf("%d달의 마지막 날은 %d입니다\n", month[j],
+		 cal.getMaxDaysOfMonth(month[j]));
+		 }
+		
 	}
 
 }
